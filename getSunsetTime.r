af33ca0a236c57e29f1e3b4870c07e590dd14277
Andrew Su <andrew.su@gmail.com>
2019-01-05 15:51:55 -0800

    make executable

--- getSunsetTime.r
+++ getSunsetTime.r
@@ -1,3 +1,5 @@
+#!/usr/bin/Rscript
+
 library(suncalc)
 #cat(getSunlightTimes(date = Sys.Date(), lat = 32.896244, lon = -117.242651, tz = "")$sunset)
 #cat(format((getSunlightTimes(date = Sys.Date(), lat = 32.896244, lon = -117.242651, tz = "")$sunset),'%Y%m%d%H%M'))
