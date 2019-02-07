5df200a2285e26750d5616bad93d1610c28c838e
Andrew Su <andrew.su@gmail.com>
2019-02-07 10:14:35 -0800

    set exposure based on top half of image only

--- getBestShutter.sh
+++ getBestShutter.sh
@@ -17,12 +17,24 @@ fi
 source $CONFIG_FILE
 
 lastNumColors=0
-for ((i=36;i>=0;i=i-2)); do
+
+# iterate through shutterspeed settings, starting with longest exposure, decreasing by 2/3 stop each time
+for ((i=8;i>=0;i=i-2)); do
     gphoto2 --set-config shutterspeed=$i
     gphoto2 --capture-image-and-download --force-overwrite --filename $ROOT/tmp/test.jpg
-    numColors=`identify -format %k $ROOT/tmp/test.jpg`
-    echo "$i: $numColors"
-    rm $ROOT/tmp/test.jpg 
+
+    # because we're looking at sunset, let's create a new with with just the top half of the photo
+    HEIGHT=`identify -format %h $ROOT/tmp/test.jpg`
+    WIDTH=`identify -format %w $ROOT/tmp/test.jpg`
+    TOP=`expr $HEIGHT / 2`
+    convert -crop ${WIDTH}x${TOP}+0+0 $ROOT/tmp/test.jpg $ROOT/tmp/test_top.jpg
+
+    # use imagemagick to get number of unique colors -- see https://imagemagick.org/script/escape.php
+    numColors=`identify -format %k $ROOT/tmp/test_top.jpg`
+    echo "Shutter speed setting $i has $numColors unique colors"
+    rm $ROOT/tmp/test.jpg $ROOT/tmp/test_top.jpg
+
+    # test if the number of unique colors has decreased; if so, stop and use last setting
     if (( $numColors < $lastNumColors )); then
         break
     fi
