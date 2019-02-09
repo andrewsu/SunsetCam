fa17263be6f16060088dbf44a72fd2cfec3f3c17
Andrew Su <andrew.su@gmail.com>
2019-02-08 17:06:10 -0800

    add sunrise script

---
+++ getSunriseTime.r
@@ -0,0 +1,6 @@
+#!/usr/bin/Rscript
+
+library(suncalc)
+#cat(getSunlightTimes(date = Sys.Date(), lat = 32.896244, lon = -117.242651, tz = "")$sunset)
+#cat(format((getSunlightTimes(date = Sys.Date(), lat = 32.896244, lon = -117.242651, tz = "")$sunset),'%Y%m%d%H%M'))
+cat(format((getSunlightTimes(date = Sys.Date(), lat = 32.896244, lon = -117.242651, tz = "")$sunrise),'%Y-%m-%d %H:%M:%S %Z'))
