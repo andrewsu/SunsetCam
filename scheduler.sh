b244a3e626b1c0fbeee49dce7a2cf791f3b83406
Andrew Su <andrew.su@gmail.com>
2019-02-11 21:04:57 -0800

    move 5 mins earlier, +1 stop compensation

--- scheduler.sh
+++ scheduler.sh
@@ -21,14 +21,14 @@ echo "sunset: $sunset" >> $LOG_FILE
 
 ### the above commands should be used to tun this script at a certain time using 'at'
 
-executionTime=`date -d "$sunset -20 min" +"%Y%m%d%H%M"`
+executionTime=`date -d "$sunset -25 min" +"%Y%m%d%H%M"`
 echo "`date`: Logging command to execute at: $executionTime" >> $LOG_FILE
 
 
 ### schedule photo capture
 
 #echo "echo '`date`: Executing photo capture' >> /home/pi/SunsetCam/log" | at -t $executionTime
-echo "bash /home/pi/SunsetCam/SunsetCam.sh -e 1 -d 1 >> $LOG_FILE" | at -t $executionTime
+echo "bash /home/pi/SunsetCam/SunsetCam.sh -e 1 -d 1 -c 18 >> $LOG_FILE" | at -t $executionTime
 
 
 ### GET SUNRISE TIME
