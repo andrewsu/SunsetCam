408d687f3ca84f546804803505cb0d5dac67b4ae
Andrew Su <andrew.su@gmail.com>
2019-02-22 19:51:20 -0800

    use entire image

--- getBestShutter.sh
+++ getBestShutter.sh
@@ -25,16 +25,10 @@ for ((i=36;i>=0;i=i-2)); do
     gphoto2 --set-config shutterspeed=$i
     gphoto2 --quiet --capture-image-and-download --force-overwrite --filename $ROOT/tmp/test.jpg
 
-    # because we're looking at sunset, let's create a new with with just the top half of the photo
-    HEIGHT=`identify -format %h $ROOT/tmp/test.jpg`
-    WIDTH=`identify -format %w $ROOT/tmp/test.jpg`
-    TOP=`expr $HEIGHT / 2`
-    convert -crop ${WIDTH}x${TOP}+0+0 $ROOT/tmp/test.jpg $ROOT/tmp/test_top.jpg
-
     # use imagemagick to get number of unique colors -- see https://imagemagick.org/script/escape.php
-    numColors=`identify -format %k $ROOT/tmp/test_top.jpg`
+    numColors=`identify -format %k $ROOT/tmp/test.jpg`
     echo "Shutter speed setting $i has $numColors unique colors" >> $LOG_FILE
-    rm $ROOT/tmp/test.jpg $ROOT/tmp/test_top.jpg
+    rm $ROOT/tmp/test.jpg
 
     # test if the number of unique colors has decreased; if so, stop and use last setting
     if (( $numColors < $lastNumColors )); then
